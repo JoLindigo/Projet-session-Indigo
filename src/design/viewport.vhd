e0ba1f3aeef0c57d8ec4ce546099abd708b13d09
Xelif88 <felixantoinebegin@gmail.com>
2025-03-23 18:16:18 -0400

    Screen size 1024 x 1024 and add clk

--- src/design/viewport.vhd
+++ src/design/viewport.vhd
@@ -24,64 +24,58 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity viewport is
-    Port ( i_print_px_y : in STD_LOGIC_VECTOR (9 downto 0);
-           i_print_px_x : in STD_LOGIC_VECTOR (9 downto 0);
-           i_offset_y : in STD_LOGIC_VECTOR (9 downto 0);
-           i_offset_x : in STD_LOGIC_VECTOR (9 downto 0);
-           o_global_x : out STD_LOGIC_VECTOR (9 downto 0);
-           o_global_y : out STD_LOGIC_VECTOR (9 downto 0));
+    Port ( i_clk            : in std_logic;
+           i_reset          : in std_logic;
+           i_print_px_y     : in STD_LOGIC_VECTOR (11 downto 0);
+           i_print_px_x     : in STD_LOGIC_VECTOR (11 downto 0);
+           i_offset_y       : in STD_LOGIC_VECTOR (9 downto 0);
+           i_offset_x       : in STD_LOGIC_VECTOR (9 downto 0);
+           i_ch_offset_en   : in std_logic;
+           o_global_x       : out STD_LOGIC_VECTOR (11 downto 0);
+           o_global_y       : out STD_LOGIC_VECTOR (11 downto 0));
 end viewport;
 
 architecture Behavioral of viewport is
+    constant FULL_WIDTH  : unsigned(9 downto 0) := to_unsigned(1023, 10);
+    constant FULL_HEIGHT  : unsigned(9 downto 0) := to_unsigned(1023, 10);
     
-    constant SCREEN_WIDTH  : unsigned(9 downto 0) := to_unsigned(640, 10); -- Remplacez par la largeur réelle
-    constant SCREEN_HEIGHT : unsigned(9 downto 0) := to_unsigned(360, 10);  -- Remplacez par la hauteur réelle
+--    constant VIEWPORT_WIDTH  : unsigned(9 downto 0) := to_unsigned(640, 10); -- Remplacez par la largeur réelle
+--    constant VIEWPORT_HEIGHT : unsigned(9 downto 0) := to_unsigned(360, 10);  -- Remplacez par la hauteur réelle
     
     signal sum_x, sum_y : std_logic_vector(11 downto 0);
-    signal carry : std_logic_vector(1 downto 0);
     
-    signal extended_x, extended_y, extended_offset_x, extended_offset_y : std_logic_vector(11 downto 0);
-    
+    signal s_offset_x : std_logic_vector(9 downto 0) := (others => '0');
+    signal s_offset_y : std_logic_vector(9 downto 0) := (others => '0');
 begin
+    sum_x <= std_logic_vector(unsigned(i_print_px_x) + unsigned(s_offset_x));
+    sum_y <= std_logic_vector(unsigned(i_print_px_y) + unsigned(s_offset_y));
     
-    -- Extension des entrées 10 bits en 12 bits
-    extended_x        <= "00" & i_print_px_x;
-    extended_y        <= "00" & i_print_px_y;
-    extended_offset_x <= "00" & i_offset_x;
-    extended_offset_y <= "00" & i_offset_y;
+    process(i_clk, i_reset)
+    begin
+        if i_reset = '1' then
+           s_offset_x <= (others => '0');
+           s_offset_y <= (others => '0');
+        elsif rising_edge(i_clk) then
+            if i_ch_offset_en = '1' then
+                s_offset_x <= i_offset_x;
+                s_offset_y <= i_offset_y;
+            end if;
+        end if;      
+    end process;
     
-    UX : entity work.Add12bits
-        port map(
-           X => extended_x,
-           Y => extended_offset_x,
-           Cin => '0',
-           Sum => sum_x,
-           Cout => carry(0)
-        );
-        
-    UY : entity work.Add12bits
-        port map(
-           X => extended_y,
-           Y => extended_offset_y,
-           Cin => '0',
-           Sum => sum_y,
-           Cout => carry(1)
-        );
-
     process(sum_x, sum_y)
     begin
         -- Vérification des limites
-        if unsigned(sum_x(9 downto 0)) >= SCREEN_WIDTH then
-            o_global_x <= std_logic_vector(TO_UNSIGNED(to_integer(unsigned(sum_x(9 downto 0))) - to_integer(SCREEN_WIDTH), 10));
+        if unsigned(sum_x) > FULL_WIDTH then
+            o_global_x <= std_logic_vector(unsigned(sum_x) - FULL_WIDTH);
         else
-            o_global_x <= sum_x(9 downto 0);
+            o_global_x <= sum_x;
         end if;
         
-        if unsigned(sum_y(9 downto 0)) >= SCREEN_HEIGHT then
-            o_global_y <= std_logic_vector(TO_UNSIGNED(to_integer(unsigned(sum_y(9 downto 0))) - to_integer(SCREEN_HEIGHT), 10));
+        if unsigned(sum_y) > FULL_HEIGHT then
+            o_global_y <= std_logic_vector(unsigned(sum_y) - FULL_HEIGHT);
         else
-            o_global_y <= sum_y(9 downto 0);
+            o_global_y <= sum_y;
         end if;
     end process;
-    
 end Behavioral;
--- src/simulation/viewport_tb.vhd
+++ src/simulation/viewport_tb.vhd
@@ -30,69 +30,90 @@ architecture Behavioral of viewport_tb is
     
     -- Component declaration
     component viewport
-        Port (
-            i_print_px_y : in STD_LOGIC_VECTOR (9 downto 0);
-            i_print_px_x : in STD_LOGIC_VECTOR (9 downto 0);
-            i_offset_y   : in STD_LOGIC_VECTOR (9 downto 0);
-            i_offset_x   : in STD_LOGIC_VECTOR (9 downto 0);
-            o_global_x   : out STD_LOGIC_VECTOR (9 downto 0);
-            o_global_y   : out STD_LOGIC_VECTOR (9 downto 0)
-        );
+        Port ( i_clk            : in std_logic;
+               i_reset          : in std_logic;
+               i_print_px_y     : in STD_LOGIC_VECTOR (11 downto 0);
+               i_print_px_x     : in STD_LOGIC_VECTOR (11 downto 0);
+               i_offset_y       : in STD_LOGIC_VECTOR (9 downto 0);
+               i_offset_x       : in STD_LOGIC_VECTOR (9 downto 0);
+               i_ch_offset_en   : in std_logic;
+               o_global_x       : out STD_LOGIC_VECTOR (11 downto 0);
+               o_global_y       : out STD_LOGIC_VECTOR (11 downto 0));
     end component;
     
     -- Signals
-    signal i_print_px_y, i_print_px_x : STD_LOGIC_VECTOR (9 downto 0);
+    signal i_print_px_y, i_print_px_x : STD_LOGIC_VECTOR (11 downto 0);
     signal i_offset_y, i_offset_x : STD_LOGIC_VECTOR (9 downto 0);
-    signal o_global_x, o_global_y : STD_LOGIC_VECTOR (9 downto 0);
+    signal o_global_x, o_global_y : STD_LOGIC_VECTOR (11 downto 0);
+    signal i_clk, i_reset, i_ch_offset_en: STD_LOGIC;
     
-    -- Constants
-    constant SCREEN_WIDTH  : unsigned(9 downto 0) := to_unsigned(640, 10);
-    constant SCREEN_HEIGHT : unsigned(9 downto 0) := to_unsigned(360, 10);
+--    -- Constants
+--    constant SCREEN_WIDTH  : unsigned(9 downto 0) := to_unsigned(640, 10);
+--    constant SCREEN_HEIGHT : unsigned(9 downto 0) := to_unsigned(360, 10);
+    
+    constant PERIOD         : time :=  13.531531 ns;
+    constant COMMAND_PERIOD : time :=  50 ns;
     
 begin
     
     -- Instantiate the viewport module
     uut: viewport
         port map(
-            i_print_px_y => i_print_px_y,
-            i_print_px_x => i_print_px_x,
-            i_offset_y   => i_offset_y,
-            i_offset_x   => i_offset_x,
-            o_global_x   => o_global_x,
-            o_global_y   => o_global_y
+           i_clk            => i_clk,
+           i_reset          => i_reset,
+           i_print_px_y     => i_print_px_y,
+           i_print_px_x     => i_print_px_x,
+           i_offset_y       => i_offset_y,
+           i_offset_x       => i_offset_x,
+           i_ch_offset_en   => i_ch_offset_en,
+           o_global_x       => o_global_x,
+           o_global_y       => o_global_y    
         );
+    sim_clk_p: process
+        begin
+          i_clk <= '1';
+          loop
+             wait for PERIOD / 2;
+             i_clk <= not i_clk;
+          end loop;
+    end process;
     
     -- Test process
     stim_proc: process
     begin
+        i_reset <= '0';
         
         -- Test Case 1: Normal Addition (within limits)
-        i_print_px_x <= std_logic_vector(to_unsigned(400, 10));
-        i_offset_x   <= std_logic_vector(to_unsigned(100, 10));
-        i_print_px_y <= std_logic_vector(to_unsigned(200, 10));
-        i_offset_y   <= std_logic_vector(to_unsigned(50, 10));
-        wait for 10 ns;
+        i_print_px_x    <= std_logic_vector(to_unsigned(800, 12));
+        i_offset_x      <= std_logic_vector(to_unsigned(100, 10));
+        i_print_px_y    <= std_logic_vector(to_unsigned(700, 12));
+        i_offset_y      <= std_logic_vector(to_unsigned(50, 10));
+        i_ch_offset_en  <= '1';
+        wait for COMMAND_PERIOD;
         
         -- Test Case 2: Exceeding X limit
-        i_print_px_x <= std_logic_vector(to_unsigned(600, 10));
-        i_offset_x   <= std_logic_vector(to_unsigned(100, 10));
-        i_print_px_y <= std_logic_vector(to_unsigned(200, 10));
-        i_offset_y   <= std_logic_vector(to_unsigned(50, 10));
-        wait for 10 ns;
+        i_print_px_x    <= std_logic_vector(to_unsigned(800, 12));
+        i_offset_x      <= std_logic_vector(to_unsigned(600, 10));
+        i_print_px_y    <= std_logic_vector(to_unsigned(700, 12));
+        i_offset_y      <= std_logic_vector(to_unsigned(50, 10));
+        i_ch_offset_en  <= '1';
+        wait for COMMAND_PERIOD;
         
         -- Test Case 3: Exceeding Y limit
-        i_print_px_x <= std_logic_vector(to_unsigned(400, 10));
-        i_offset_x   <= std_logic_vector(to_unsigned(100, 10));
-        i_print_px_y <= std_logic_vector(to_unsigned(350, 10));
-        i_offset_y   <= std_logic_vector(to_unsigned(20, 10));
-        wait for 10 ns;
+        i_print_px_x    <= std_logic_vector(to_unsigned(800, 12));
+        i_offset_x      <= std_logic_vector(to_unsigned(100, 10));
+        i_print_px_y    <= std_logic_vector(to_unsigned(700, 12));
+        i_offset_y      <= std_logic_vector(to_unsigned(900, 10));
+        i_ch_offset_en  <= '1';
+        wait for COMMAND_PERIOD;
         
         -- Test Case 4: Exceeding Both Limits
-        i_print_px_x <= std_logic_vector(to_unsigned(630, 10));
-        i_offset_x   <= std_logic_vector(to_unsigned(50, 10));
-        i_print_px_y <= std_logic_vector(to_unsigned(355, 10));
-        i_offset_y   <= std_logic_vector(to_unsigned(10, 10));
-        wait for 10 ns;
+        i_print_px_x    <= std_logic_vector(to_unsigned(800, 12));
+        i_offset_x      <= std_logic_vector(to_unsigned(600, 10));
+        i_print_px_y    <= std_logic_vector(to_unsigned(700, 12));
+        i_offset_y      <= std_logic_vector(to_unsigned(900, 10));
+        i_ch_offset_en  <= '1';
+        wait for COMMAND_PERIOD;
         
         -- End simulation
         wait;
